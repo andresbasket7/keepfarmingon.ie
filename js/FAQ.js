43cfc67e991318008553c9501dd98e208cce5de4
andresbasket7 <andresbasket77@gmail.com>
2025-08-28 14:26:57 +0200

    Se realiza la página de FAQ con los dos textos del FrenchMan. Se reduce el tamño de todas las imágenes nuevas para la carga de la página. Se añade JS para los botones de FAQ.

---
+++ js/FAQ.js
@@ -0,0 +1,11 @@
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+}
+
+// FAQ toggle
+document.querySelectorAll('.faq-question').forEach(item => {
+  item.addEventListener('click', () => {
+    const answer = item.nextElementSibling;
+    answer.style.display = answer.style.display === "block" ? "none" : "block";
+  });
+});
